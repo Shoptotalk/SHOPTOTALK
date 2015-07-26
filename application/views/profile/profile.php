3dcbb488b4b9ce5dc2983ae5e8b49bde238002eb
Yossi Shaish <yshaish3@gmail.com>
2015-07-26 13:42:08 +0300

    friends relations

--- application/views/profile/profile.php
+++ application/views/profile/profile.php
@@ -7,6 +7,7 @@
                 <div class="fixed profile_fixed" style="display: none;">
                     <div><img src="<?= $user->profilePicture ?>" alt="" class="max-profile-picture img-responsive"></div>
                     <div class="text-center"><h3><?= $user->fname ?> <?= $user->lname ?></h3></div>
+                    <div class="btn btn-success btn-block text-center p10 mb10" id="friendsBtn" onclick="friends(<?= $user->_id ?>)">Be friend with <?= $user->fname ?> <?= $user->lname ?></div>
                     <div class="btn btn-primary btn-block text-center p10 mb10"><?= $user->fname ?> have <strong><?= $count_user_friends ?></strong> friends</div>
                     <div class="btn btn-primary btn-block text-center p10 mb10">Published <strong><?= $count_user_posts ?></strong> posts</div>
                     <div class="btn btn-primary btn-block text-center p10 mb10">Loved <strong><?= $count_user_loves ?></strong> posts</div>
@@ -19,3 +20,18 @@
     </div>
     <div class="col-sm-1 col-lg-3"></div>
 </div>
+
+<!-- Post Modal -->
+<button type="button" class="btn btn-primary" id="PostButton" data-toggle="modal" data-target="#PostModal" style="display:none;"></button>
+<div class="modal fade" id="PostModal" tabindex="-1" role="dialog" aria-labelledby="PostModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="PostModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="PostModalBody"></div>
+    </div>
+  </div>
+</div>
+<!-- Post Modal -->
--- assets/js/profile_show.js
+++ assets/js/profile_show.js
@@ -10,6 +10,16 @@ function initializeProfileShow() {
     $(".profile_fixed").fadeIn();
 }
 
+function friends(friend_id) {
+	$.post("/friends/changeRelationWith", {friend_id : friend_id}, function(data){
+		var json = JSON.parse(data);
+		if(json.success == "successful") {
+			if(json.friends) $("#friendsBtn").html("Unfriend with " + json.friendName);
+				else $("#friendsBtn").html("Be friend with " + json.friendName);
+		}
+	});
+}
+
 $(window, document).on("resize", function(){
     initializeProfileShow();
 });
